3fdb9fd3893bf4be6ebab430809c23c2fa3ed88e
hknhtpgl48 <hkn.hatipolu48@gmail.com>
2024-01-02 21:12:49 +0300

    guessTheFlag design completed

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Hakan Hatipoğlu on 1.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
